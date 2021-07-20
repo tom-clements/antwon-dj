9b025b52ec5fdeab312a44cdb145eaea58626886
Taylor Siviter <taylor@siviter.xyz>
2021-07-20 22:36:12 +0100

    feat: add dispatch and selector helpers to store

--- client/model/Store.ts
+++ client/model/Store.ts
@@ -18,3 +18,6 @@ export function createStore(preloadedState: any) {
 export type Store = ReturnType<typeof createStore>;
 export type RootState = ReturnType<Store["getState"]>;
 export type AppDispatch = Store["dispatch"];
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
