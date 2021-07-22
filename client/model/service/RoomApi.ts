9b104b19540faeaeb4bc2c3173a09e8b228dcf57
Taylor Siviter <taylor@siviter.xyz>
2021-07-23 00:46:36 +0100

    fix: update getRoomIdByCode contract

--- client/model/service/RoomApi.ts
+++ client/model/service/RoomApi.ts
@@ -1,6 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { getBaseUrl } from "model/service/ApiGateway";
-import { selectFromJson } from "model/service/SafeJsonParse";
 
 interface RoomIdByCodeResponseBodyDto {
     "room_guid": string;
@@ -8,11 +7,9 @@ interface RoomIdByCodeResponseBodyDto {
 
 interface RoomIdByCodeResponseDto {
     status: 200;
-    body: string;
+    body: RoomIdByCodeResponseBodyDto;
 }
 
-function isRoomIdByCodeResponseBody(obj: any): obj is RoomIdByCodeResponseBodyDto {
-    return "room_guid" in obj;
 }
 
 export const roomApi = createApi({
@@ -22,7 +19,7 @@ export const roomApi = createApi({
     endpoints: (builder) => ({
         getRoomIdByCode: builder.query<string, string>({
             query: code => ({ url: `room?room_code=${code}` }),
-            transformResponse: (response: RoomIdByCodeResponseDto) => selectFromJson(isRoomIdByCodeResponseBody, response.body, r => r.room_guid),
+            transformResponse: (response: RoomIdByCodeResponseDto) => response.body.room_guid,
             providesTags: ["Room"],
         }),
     }),
