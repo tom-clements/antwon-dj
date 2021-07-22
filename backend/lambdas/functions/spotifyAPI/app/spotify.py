f150a0e56752e283a0948b45170ab9227fa47acc
Tom Clements <tomclem171@googlemail.com>
2021-07-22 22:37:52 +0100

    feat: adding layers

---
+++ backend/lambdas/functions/spotifyAPI/app/spotify.py
@@ -0,0 +1,76 @@
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy
+from spotipy import oauth2
+import base64
+import urllib
+import json
+import requests
+import app
+from functools import wraps
+
+
+API_URL = "https://m5ua2jc51a.execute-api.eu-west-2.amazonaws.com/dev"
+REDIRECT_URI = f'{API_URL}/spotifyCallback'
+SCOPES = 'playlist-read-collaborative user-modify-playback-state user-read-playback-state user-read-private playlist-modify-private playlist-modify-public'
+
+def get_spotify_secrets():
+    secrets = app.get_secret('spotify_client')
+    return secrets
+
+def app_Authorization():
+    secrets = get_spotify_secrets()
+    f = {'response_type': 'code', 'client_id': secrets['SPOTIFY_CLIENT_ID'], 'scope': SCOPES, 'redirect_uri': REDIRECT_URI}#, state: state}
+    url = 'https://accounts.spotify.com/authorize?{}'.format(urllib.parse.urlencode(f))
+    return url
+
+def get_spotify(auth):
+    secrets = get_spotify_secrets()
+    client_credentials_manager = SpotifyClientCredentials(client_id=secrets['SPOTIFY_CLIENT_ID'], client_secret=secrets['SPOTIFY_CLIENT_SECRET'])
+    sp = spotipy.Spotify(auth=auth, client_credentials_manager=client_credentials_manager)
+    return sp
+
+
+def get_token(code):
+    secrets = get_spotify_secrets()
+    data = {'code': code, 'redirect_uri': REDIRECT_URI, 'grant_type': 'authorization_code'}#, state: state}
+    url = 'https://accounts.spotify.com/api/token'
+    base64encoded = base64.b64encode(bytes("{}:{}".format(secrets['SPOTIFY_CLIENT_ID'], secrets['SPOTIFY_CLIENT_SECRET']), 'utf-8'))
+    headers= {'Authorization': 'Basic {}'.format(str(base64encoded, 'utf-8'))}
+    res = requests.post(url, headers=headers, data=data)
+    return json.loads(res.text)  
+    
+    
+def refresh_spotify_token(refresh_token):
+    secrets = get_spotify_secrets()
+    sp_oauth = oauth2.SpotifyOAuth(
+        client_id=secrets['SPOTIFY_CLIENT_ID'],
+        client_secret=secrets['SPOTIFY_CLIENT_SECRET'],
+        redirect_uri=REDIRECT_URI,
+        scope=SCOPES,
+    )
+    # if sp_oauth.is_token_expired({'access_token': access_token, 'refresh_token': refresh_token}):
+    token_info = sp_oauth.refresh_access_token(refresh_token)
+    token = token_info['access_token']
+    return token
+    
+
+def get_spotify_session(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        user_id = app.db_queries.get_room_id_from_guid(kwargs['room_guid'])
+        spotify_user = app.db_queries.get_spotify_user_details(user_id)
+        try:
+            spotify_session = get_spotify(auth=spotify_user['spotify_access_token'])
+            return f(spotify_session, *args, **kwargs)
+        except spotipy.client.SpotifyException:
+            try:
+                token = refresh_spotify_token(spotify_user['spotify_refresh_token'])
+                app.db_queries.update_access_token(spotify_user['spotify_user_id'], token)
+                spotify_session = get_spotify(auth=token)
+                return f(spotify_session, *args, **kwargs)
+            except:
+                raise
+                return get_spotify_session(f)
+    return decorated
+    
+
