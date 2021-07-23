a2a5aded38f6fe321a97bae5b38af104db7c0fa1
Taylor Siviter <taylor@siviter.xyz>
2021-07-23 01:04:53 +0100

    feat: add room queue endpoint and rendering

--- client/components/SongQueue.tsx
+++ client/components/SongQueue.tsx
@@ -1,12 +1,50 @@
 import { FC } from "react";
+import styled from "styled-components";
 import { Flex } from "components/layout/Flex";
+import { FlexCentre } from "components/layout/FlexCentre";
+import { Spacing } from "components/layout/Spacing";
+import { Song } from "components/Song";
+import { Spinner } from "components/Spinner";
+import { roomApi } from "model/service/RoomApi";
+
+export const QueueContainer = styled.div`
+    border-width: 0.05em;
+    border-style: solid;
+    border-right: none;
+    border-bottom: none;
+    border-left: none;
+
+    border-color: ${props => props.theme.field.base.borderColor};
+`;
 
 interface Props {
     roomId: string;
 }
 
 export const SongQueue: FC<Props> = props => {
+    const result = roomApi.endpoints.getRoomQueue.useQuery(props.roomId);
+
+    const isPending = result.isLoading || result.isFetching;
+    if (isPending) return (
+        <FlexCentre>
+            <Spinner scale={3} />
+        </FlexCentre>
+    );
+
+    if (result.isError || !result.data) return (
+        <FlexCentre>
+            Could not find room queue
+        </FlexCentre>
+    );
+
     return (
-        <div>Song queue element</div>
+        <Flex flexDirection="column">
+            <Spacing marginBottom={"0.5em"}>
+                Queue:
+            </Spacing>
+            <QueueContainer>
+                {result.data.map(s => <Song key={s.id} {...s} />)}
+            </QueueContainer>
+        </Flex>
     );
 };
--- client/model/service/RoomApi.ts
+++ client/model/service/RoomApi.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { getBaseUrl } from "model/service/ApiGateway";
+import { SongDto } from "model/service/SpotifySearchApi";
 
 interface RoomIdByCodeResponseBodyDto {
     "room_guid": string;
@@ -10,17 +11,39 @@ interface RoomIdByCodeResponseDto {
     body: RoomIdByCodeResponseBodyDto;
 }
 
+interface RoomQueueResponseDto {
+    status: 200;
+    body: RoomQueueResponseBodyDto;
+}
+
+interface RoomSongDto extends SongDto {
+    "in_active": boolean;
+    "is_played": boolean;
+    "is_removed": boolean;
+    "insert_time": Date;
+}
+
+interface RoomQueueResponseBodyDto {
+    "room_queue": RoomSongDto[];
+}
+
+function isRoomQueueResponseBody(obj: any): obj is RoomQueueResponseBodyDto {
+    return "room_queue" in obj;
 }
 
 export const roomApi = createApi({
     reducerPath: "roomApi",
     baseQuery: fetchBaseQuery({ baseUrl: getBaseUrl() }),
-    tagTypes: ["Room"],
+    tagTypes: ["Room", "RoomQueue"],
     endpoints: (builder) => ({
         getRoomIdByCode: builder.query<string, string>({
             query: code => ({ url: `room?room_code=${code}` }),
             transformResponse: (response: RoomIdByCodeResponseDto) => response.body.room_guid,
             providesTags: ["Room"],
+        }),
+        getRoomQueue: builder.query<RoomSongDto[], string>({
+            query: roomId => ({ url: `roomQueue?room_guid=${roomId}` }),
+            transformResponse: (response: RoomQueueResponseDto) => response.body.room_queue,
         }),
     }),
 });
