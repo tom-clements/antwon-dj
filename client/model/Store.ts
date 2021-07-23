8f12a6cef116b499b80710cb3bbba3c3d6e1d158
Taylor Siviter <taylor@siviter.xyz>
2021-07-23 01:11:18 +0100

    feat: add currently playing api

--- client/model/Store.ts
+++ client/model/Store.ts
@@ -2,6 +2,7 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import { RoomPortalSlice } from "model/RoomPortalSlice";
 import { UserSlice } from "model/User";
+import { spotifyCurrentlyPlayingApi } from "model/service/SpotifyCurrentlyPlaying";
 import { spotifySearchApi } from "model/service/SpotifySearchApi";
 import { roomApi } from "model/service/RoomApi";
 
@@ -10,10 +11,12 @@ export function createStore(preloadedState: any) {
         reducer: {
             roomPortal: RoomPortalSlice.reducer,
             user: UserSlice.reducer,
+            [spotifyCurrentlyPlayingApi.reducerPath]: spotifyCurrentlyPlayingApi.reducer,
             [spotifySearchApi.reducerPath]: spotifySearchApi.reducer,
             [roomApi.reducerPath]: roomApi.reducer,
         },
         middleware: getDefaultMiddleware => getDefaultMiddleware().concat(...[
+            spotifyCurrentlyPlayingApi.middleware,
             spotifySearchApi.middleware,
             roomApi.middleware,
         ]),
---
+++ client/model/service/SpotifyCurrentlyPlaying.ts
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { getBaseUrl } from "model/service/ApiGateway";
+import { SongDto } from "model/service/SpotifySearchApi";
+
+interface SongsForSearchResponseDto {
+    status: 200;
+    "song": SongDto;
+}
+
+export const spotifyCurrentlyPlayingApi = createApi({
+    reducerPath: "spotifyCurrentlyPlayingApi",
+    baseQuery: fetchBaseQuery({ baseUrl: getBaseUrl() }),
+    tagTypes: ["SpotifyCurrentlyPlaying"],
+    endpoints: (builder) => ({
+        get: builder.query<SongDto, string>({
+            query: roomId => ({ url: `spotifyCurrentlyPlaying?room_guid=${roomId}` }),
+            transformResponse: (response: SongsForSearchResponseDto) => response.song,
+            providesTags: ["SpotifyCurrentlyPlaying"],
+        }),
+    }),
+});
