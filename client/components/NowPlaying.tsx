1e9f75c0b7a162df7efb7d7c346237dc4c11c9fa
Taylor Siviter <taylor@siviter.xyz>
2021-07-23 01:25:33 +0100

    feat: add now playing UI

--- client/components/NowPlaying.tsx
+++ client/components/NowPlaying.tsx
@@ -1,12 +1,45 @@
 import { FC } from "react";
+import styled from "styled-components";
 import { Flex } from "components/layout/Flex";
+import { FlexCentre } from "components/layout/FlexCentre";
+import { Spacing } from "components/layout/Spacing";
+import { Song } from "components/Song";
+import { Spinner } from "components/Spinner";
+import { spotifyCurrentlyPlayingApi } from "model/service/SpotifyCurrentlyPlaying";
+
+
+export const NowPlayingContainer = styled.div`
+    border-top: 0.05em solid ${props => props.theme.field.base.borderColor};
+`;
 
 interface Props {
     roomId: string;
 }
 
 export const NowPlaying: FC<Props> = props => {
+    const result = spotifyCurrentlyPlayingApi.endpoints.get.useQuery(props.roomId);
+
+    const isPending = result.isLoading || result.isFetching;
+    if (isPending) return (
+        <FlexCentre>
+            <Spinner />
+        </FlexCentre>
+    );
+
+    if (result.isError || !result.data) return (
+        <FlexCentre>
+            Could not get currently playing
+        </FlexCentre>
+    );
+
     return (
-        <div>NowPlaying</div>
+        <NowPlayingContainer>
+            <Flex flexDirection="column">
+                <Spacing marginTop={"0.5em"} marginBottom={"0.5em"}>
+                    Currently playing:
+                </Spacing>
+                <Song {...result.data} />
+            </Flex>
+        </NowPlayingContainer>
     );
 };
