18fcab5f692ded713b6d3e40e257a0d98d18967d
Taylor Siviter <taylor@siviter.xyz>
2021-09-26 21:43:05 +0100

    refactor: remove unnecessary state and actions from RoomPortalSlice

--- client/model/RoomPortalSlice.ts
+++ client/model/RoomPortalSlice.ts
@@ -3,13 +3,11 @@ import type { RootState } from 'model/Store';
 
 interface RoomPortalState {
     roomCode: string | null;
-    shouldQueryRoomId: boolean;
 }
 
 function getInitialRoomPortalState(): RoomPortalState {
     return {
         roomCode: null,
-        shouldQueryRoomId: false,
     };
 }
 
@@ -18,12 +16,9 @@ export const RoomPortalSlice = createSlice({
     initialState: getInitialRoomPortalState(),
     reducers: {
         setRoomCode: (state, action: PayloadAction<string | null>) => ({ ...state, roomCode: action.payload }),
-        queryRoomId: state => ({ ...state, shouldQueryRoomId: true }),
-        stopRoomIdQuery: state => ({ ...state, shouldQueryRoomId: false }),
     },
 });
 
-export const { setRoomCode, queryRoomId, stopRoomIdQuery } = RoomPortalSlice.actions;
+export const { setRoomCode } = RoomPortalSlice.actions;
 
 export const selectRoomCode = (state: RootState) => state.roomPortal.roomCode;
-export const selectShouldQueryRoomId = (state: RootState) => state.roomPortal.shouldQueryRoomId;
