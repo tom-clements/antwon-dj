f2603a7d7b30df5737e326c4d4d2a25a060019ab
Tom Clements <tomclem171@googlemail.com>
2022-03-01 14:43:56 +0000

    feat: adding custom domain to api
    feat: adding previews

--- backend/add_pr_stage_to_chalice_config.py
+++ backend/add_pr_stage_to_chalice_config.py
@@ -11,24 +11,15 @@ def open_config() -> Dict[str, Any]:
         return json.load(f)
 
 
-def open_deployed() -> Dict[str, Any]:
-    with open(".chalice/deployed/dev.json", "r") as f:
-        return json.load(f)
-
-
 def write_config(config: Dict[str, Any]):
     with open(".chalice/config.json", "w") as f:
         json.dump(config, f, indent=4)
 
 
-def write_deployed(pr_stage, deployed: Dict[str, Any]) -> Dict[str, Any]:
-    with open(f".chalice/deployed/{pr_stage}.json", "w") as f:
-        json.dump(deployed, f, indent=4)
-
-
 def add_pr_stage_to_config(config: Dict[str, Any], pr_stage: str):
     config["stages"][pr_stage] = deepcopy(config["stages"]["dev"])
     config["stages"][pr_stage]["api_gateway_stage"] = pr_stage
+    config["stages"][pr_stage]["api_gateway_custom_domain"]["url_prefix"] = pr_stage
     return config
 
 
@@ -47,18 +38,10 @@ def configure_config(pr_stage):
     write_config(config)
 
 
-def configure_deployed(pr_stage):
-    deployed = open_deployed()
-    deployed = add_pr_stage_to_deployed(deployed, pr_stage)
-    pprint(deployed)
-    write_deployed(pr_stage, deployed)
-
-
 @click.command()
 @click.option("--pr-stage", help="The name of the stage to create in the chalice deploy file")
 def main(pr_stage):
     configure_config(pr_stage)
-    configure_deployed(pr_stage)
 
 
 if __name__ == "__main__":
