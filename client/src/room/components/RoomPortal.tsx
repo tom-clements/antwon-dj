0270c9e8ef1f130905883d7ae67f4f536bfc7c9f
Taylor Siviter <taylor@siviter.xyz>
2022-05-18 18:51:29 +0100

    chore: move RoomPortal to domain folder
    Also add note on what it is, and the confusion with the
    state slice...

--- client/src/room/components/RoomPortal.tsx
+++ client/src/room/components/RoomPortal.tsx
@@ -10,6 +10,9 @@ interface Props {
     useRoomPortal?: UseRoomPortal;
 }
 
+// TODO clarify what RoomPortal is. It is the contents of the index page, that can
+// take you to a room. The "RoomPortal" state slice is, regrettably, different. It
+// is 'sort-of' the current room. I should make this clearer.
 export const RoomPortal: FC<Props> = props => {
     const useRoomPortal = props.useRoomPortal ?? _useRoomPortal;
     const {
