325ace28db68082c4ef67a03175230971ad0a8a9
Taylor Siviter <taylor@siviter.xyz>
2022-05-23 19:36:41 +0100

    chore: add note on bad breadcrumbs

--- client/src/common/hooks/useBreadcrumbs.ts
+++ client/src/common/hooks/useBreadcrumbs.ts
@@ -12,6 +12,10 @@ export type UseBreadcrumbs = HF<void, Return>;
 export const useBreadcrumbs: UseBreadcrumbs = () => {
     const router = useRouter();
 
+    // TODO fix naive approach to this. We need to track a
+    // loose stack of last pages too - the user can browse arbitrarily.
+    // We need to either go to the last page, or if there is none and
+    // we are not root index, go to the root index.
     return {
         isRoot: router.route === '/',
         goBack: useCallback(
