400c34e587e219a4474b69e711c4f810aca4b3ad
Taylor Siviter <taylor@siviter.xyz>
2022-06-05 14:11:53 +0100

    feat: safetly hydrate the page, we don't need SSR

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -8,6 +8,7 @@ import { getDefaultTheme } from 'styles/services/getTheme';
 import { useAppStore, createEmotionCache } from 'AppSetup';
 import { ToastError } from 'toastError/components/ToastError';
 import { DependencyProvider } from 'common/components/DependencyProvider';
+import { SafeHydrate } from 'common/components/SafeHydrate';
 
 const defaultTheme = getDefaultTheme();
 const clientSideEmotionCache = createEmotionCache();
@@ -21,24 +22,26 @@ export default function App(props: MyAppProps) {
     const store = useAppStore(pageProps.initialReduxState);
 
     return (
-        <DependencyProvider>
-            <Provider store={store}>
-                <CacheProvider value={emotionCache}>
-                    <Head>
-                        <title>antwon.dj</title>
-                        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-                        <link rel="icon" type="image/png" href="/favicon.png" />
-                        <meta content="minimum-scale=1, initial-scale=1, width=device-width" name="viewport" />
-                        <meta content={defaultTheme.palette.primary.main} name="theme-color" />
-                    </Head>
-                    <ThemeProvider>
-                        <CssBaseline />
-                        <Global styles={{ 'body': { overscrollBehavior: 'contain' } }} />
-                        <Component {...pageProps} />
-                        <ToastError />
-                    </ThemeProvider>
-                </CacheProvider>
-            </Provider>
-        </DependencyProvider>
+        <SafeHydrate>
+            <DependencyProvider>
+                <Provider store={store}>
+                    <CacheProvider value={emotionCache}>
+                        <Head>
+                            <title>antwon.dj</title>
+                            <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+                            <link rel="icon" type="image/png" href="/favicon.png" />
+                            <meta content="minimum-scale=1, initial-scale=1, width=device-width" name="viewport" />
+                            <meta content={defaultTheme.palette.primary.main} name="theme-color" />
+                        </Head>
+                        <ThemeProvider>
+                            <CssBaseline />
+                            <Global styles={{ 'body': { overscrollBehavior: 'contain' } }} />
+                            <Component {...pageProps} />
+                            <ToastError />
+                        </ThemeProvider>
+                    </CacheProvider>
+                </Provider>
+            </DependencyProvider>
+        </SafeHydrate>
     );
 }
