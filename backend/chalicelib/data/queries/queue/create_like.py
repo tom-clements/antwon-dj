e73c71c0f73a60700ccaef2e7e09be1a681f2e57
Tom Clements <tomclem171@googlemail.com>
2022-06-10 23:12:01 +0100

    fix: add create time to create like

--- backend/chalicelib/data/queries/queue/create_like.py
+++ backend/chalicelib/data/queries/queue/create_like.py
@@ -1,4 +1,5 @@
 import uuid
+import datetime
 
 from sqlalchemy.orm import Session
 
@@ -9,5 +10,11 @@ from chalicelib.data.db import use_db_session
 @use_db_session(commit=True)
 def create_like(room_song_id: int, user_id: int, db_session: Session) -> None:
     db_session.add(
-        RoomSongLike(room_song_like_guid=str(uuid.uuid4()), room_song_id=room_song_id, user_id=user_id, like_value=1)
+        RoomSongLike(
+            room_song_like_guid=str(uuid.uuid4()),
+            room_song_id=room_song_id,
+            user_id=user_id,
+            like_value=1,
+            create_time=datetime.datetime.now(),
+        )
     )
